dbca18b674c31a9e8cc590b8a65d06bb9c3c4af5
KhushnudEshtemirov <khushnudeshtemirov@gmail.com>
2024-02-24 16:58:56 +0500

    Updated

--- api/server.js
+++ api/server.js
@@ -1,18 +1,14 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
-    "/expense": "/expense",
-    "/expense/:id": "/expense/:id",
-    "/income": "/income",
-    "/income/:id": "/income/:id",
+    "/expenses/:resource/:id/show": "/:resource/:id",
+    "/income/:resource/:id/show": "/:resource/:id",
   })
 );
 server.use(router);
@@ -20,5 +16,4 @@ server.listen(3000, () => {
   console.log("JSON Server is running");
 });
 
-// Export the Server API
 module.exports = server;
